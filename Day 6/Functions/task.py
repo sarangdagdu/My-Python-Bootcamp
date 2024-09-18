2a230573e4341f7f76f93dc2736fc72f199dc902
Sarang Dagdu <40080722+sarangdagdu@users.noreply.github.com>
2024-09-18 22:31:37 +0530

    Function Arguments (Position and Keywords).

--- Day 6/Functions/task.py
+++
@@ -1,5 +0,0 @@
-def my_function():
-    print("\nThis is my function\n")
-    print("Bye")
-
-my_function()
--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,9 @@
 # Functions with input
 
-def greet_with_name(name):
+def greet_with_name(name,location):
     print(f"Hello {name}")
+    print(f"What is it like in {location}")
     print(f"How do you do {name}?")
 
 
-greet_with_name("Jack Bauer")
+greet_with_name(location="Arizona",name="Sharon")

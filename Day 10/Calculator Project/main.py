194e78fe948db93ddbb2611e09b9a3f850925afe
Sarang Dagdu <40080722+sarangdagdu@users.noreply.github.com>
2024-09-24 23:15:27 +0530

    Calculator Implementation I

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -14,14 +14,29 @@ operations = { "+":add,"-":subtract,"*":multiply,"/":divide}
 
 # print(operations["*"](5,4))
 
-should_continue = False
+should_continue = True
 num_1 = float(input("What's the first number?:\n"))
 
-operation = input("Pick an Operation:\n")
-num_2 = float(input("What's the next number?:\n"))
+while should_continue == True:
 
-result = operations[operation](num_1,num_2)
-print(f"{num_1} {operation} {num_2} = {result}")
+    operation = input("Pick an Operation:\n")
+    num_2 = float(input("What's the next number?:\n"))
 
-print(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation:\n")
-should_continue = input()
+    result = operations[operation](num_1,num_2)
+    print(f"{num_1} {operation} {num_2} = {result}")
+
+    continue_prompt = input((f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation:\n"))
+
+    if bool(continue_prompt) == True:
+        should_continue = False
+
+        operation = input("Pick an Operation:\n")
+        num_2 = float(input("What's the next number?:\n"))
+
+        temp_result = operations[operation](result,num_2)
+        print(f"{result} {operation} {num_2} = {temp_result}")
+    else:
+        print("\n*50")
+
+
+
